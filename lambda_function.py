3b01a4748c6f5996c696601bb8f29d99f78ea40c
parth <prparikh02@gmail.com>
2019-03-09 10:02:49 -0500

    Minimal Lambda Handler and CodePipeline setup

--- lambda_function.py
+++ lambda_function.py
@@ -1,2 +1,8 @@
-def lambda_handler():
-    print('Hello World!')
+def lambda_handler(event, context):
+    session = event['session']
+    request = event['request']
+    print(get_user_id(session))
+
+
+def get_user_id(session):
+    return session['user']['userId']
