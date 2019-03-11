ec19de1eacb415effe6173542d874bd5ad17f768
parth <prparikh02@gmail.com>
2019-03-10 20:46:01 -0400

    Implement proper responses

--- lambda_function.py
+++ lambda_function.py
@@ -14,7 +14,7 @@ requests_table = dynamodb.Table('AlexaSkillKVStore-Requests-NA')
 data_table = dynamodb.Table('AlexaSkillKVStore-UserData-NA')
 
 
-class IntentHandler(object):
+class AbstractIntentHandler(object):
     SLOT_INFO_KEY = 'info_key'
     SLOT_INFO_VALUE = 'info_value'
 
@@ -25,10 +25,23 @@ class IntentHandler(object):
         return request['intent']['slots'][slot_name]['value']
 
 
-class PutKeyValueIntentHandler(IntentHandler):
+class PutKeyValueIntentHandler(AbstractIntentHandler):
     def handle(self, user_id, request):
+        item = self._create_user_data_item(user_id, request)
         data_table.put_item(
-            Item=self._create_user_data_item(user_id, request)
+            Item=item
+        )
+
+        response_text = 'Saving the value {} as {}'.format(
+            item[INFO_VALUE],
+            item[INFO_KEY]
+        )
+        response_card_title = 'Saving value as {}'.format(item[INFO_KEY])
+
+        return create_response(
+            speech_text=response_text,
+            card_content=response_text,
+            card_title=response_card_title
         )
 
     def _create_user_data_item(self, user_id, request):
@@ -41,15 +54,45 @@ class PutKeyValueIntentHandler(IntentHandler):
         }
 
 
-class GetValueIntentHandler(IntentHandler):
+class GetValueIntentHandler(AbstractIntentHandler):
     def handle(self, user_id, request):
-        response = data_table.get_item(
+        info_key = self._get_slot_value(self.SLOT_INFO_KEY, request)
+        ddb_response = data_table.get_item(
             Key={
-                USER_KEY: user_id,
-                INFO_KEY: self._get_slot_value(self.SLOT_INFO_KEY, request)
+                USER_ID: user_id,
+                INFO_KEY: info_key
             }
         )
-        return response['item']
+
+        response_text = 'The value stored for {} is {}'.format(
+            info_key,
+            ddb_response['Item'][INFO_VALUE]
+        )
+        response_card_title = 'Value for {}'.format(info_key)
+
+        return create_response(
+            speech_text=response_text,
+            card_content=response_text,
+            card_title=response_card_title
+        )
+
+
+def create_response(speech_text, card_content, card_title):
+    return {
+        'version': '1.0',
+        'response': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': speech_text
+            },
+            'card': {
+                'type': 'Simple',
+                'content': card_content,
+                'title': card_title
+            },
+        },
+        'sessionAttributes': {}
+    }
 
 
 def resolve_intent_handler(request):
@@ -71,4 +114,4 @@ def lambda_handler(event, context):
     request = event['request']
 
     user_id = get_user_id(session)
-    resolve_intent_handler(request).handle(user_id, request)
+    return resolve_intent_handler(request).handle(user_id, request)
